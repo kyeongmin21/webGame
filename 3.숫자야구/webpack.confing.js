3319b8f50a4332e0bb202980b9ca66cc33bd56ee
kim kyeong min <kyeongmin21@naver.com>
2022-05-18 19:34:44 +0900

    vue 기본강좌 3-2 : 프로젝트 구조와 웹팩 빌드 - main.js에서 vue를 mount 시켜준다 - main.js랑 NumberBaseball.vue을 합쳐서 html에서는 script로 연결해준다. - package.json 에서 script 부분을 "server": "webpack" 이거 뜻이 웹팩을 돌리는데 npm run server 로 적어야 한다. - 노드가 만들어둔 스크립트 (const path = require('path')) - npm i vue-loader -D - npm i vue-templete-compiler -D

--- 3.숫자야구/webpack.confing.js
+++ 3.숫자야구/webpack.confing.js
@@ -1,16 +1,22 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const path = require('path')
+
 module.exports = {
     entry: {
-        app: './main.js'
+        app: path.join(__dirname, 'main.js')
     },
     module: {
         rules: [{
-
+            test:/\.vue$/,
+            loader: 'vue-loader'
         }]
     },
-    plugins: [],
+    plugins: [
+        new VueLoaderPlugin()
+    ],
     output: {
-        filename: 'app.js',
-        path: './dist'
+        filename: '[name].js',
+        path: path.join(__dirname, 'dist')
     }
 }
 
