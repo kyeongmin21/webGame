ef3da6f2699659399fb96dbaa9433fc0fde65b46
kim kyeong min <kyeongmin21@naver.com>
2022-06-14 19:19:47 +0900

    vue 기본강좌 6 -1 : 로또 게임 셋팅하기

---
+++ 6.로또/LottoBall.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h3>당첨 숫자</h3>
+    <div id="result">
+      <LottoBall v-for="ball in winBalls"/>
+    </div>
+    <h3>보너스</h3>
+    <LottoBall v-if="bonus"/>
+    <button v-if="redo">한번더</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LottoBall',
+  components: {},
+  data() {
+    return {
+      winBalls: [],
+      bonus: null,
+      redo: false,
+
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {}
+}
+</script>
+
+<style>
+
+</style>
